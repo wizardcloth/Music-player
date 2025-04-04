7d76b3c81f5961b4629c2d59d9593b3d4ba291ed
wizardcloth <ujjwal.chauhan8130@gmail.com>
2025-04-05 00:47:55 +0530

    testing done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-// import { AuthProvider } from "./stores/useAuthStore.tsx";
-import AuthProvider  from "./AuthProvider/adminprovider.tsx";
+import AuthProvider from "./AuthProvider/adminprovider.tsx";
+import { StrictMode } from "react";
 createRoot(document.getElementById("root")!).render(
-    // <StrictMode>
-    <BrowserRouter>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
-    </BrowserRouter>
-    // </StrictMode>
+    <StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </BrowserRouter>
+    </StrictMode>
 );
