3271675947a133d9eeaf5744d403b8cab619b219
wizardcloth <ujjwal.chauhan8130@gmail.com>
2025-04-05 00:00:25 +0530

    testing dev

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api', 
+  baseURL: 'https://music-player-psi-kohl.vercel.app/api', 
 });
 
 export default axiosInstance;
